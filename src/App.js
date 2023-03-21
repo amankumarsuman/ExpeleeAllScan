58c7a4d6e6c60e7f6a63901207b22c48a45bc7fb
akumar <akumar@comtronusa.com>
2023-03-21 04:06:21 -0400

    added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import WalletDataWithTime from "./Components/WalletDetailsWithTime";
 import { AllWalletDetails } from "./Components/AllWalletData";
+import ResponsiveAppBar from "./Components/navbars/Navbar";
 
 function App() {
   return (
     <div className="App">
       {/* <WalletDataWithTime /> */}
+      <ResponsiveAppBar />
       <AllWalletDetails />
     </div>
   );
